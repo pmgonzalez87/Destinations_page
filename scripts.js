19ffb00bb0e853bbe65a7edbfd60a96e0a1b1095
ngonpaul <pmgonzalez05@outlook.com>
2022-07-18 00:28:18 -0700

    Built skeleton for index.html, added some css and created TODOs in scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// TODO - We need to listen to what is being submitted in the form
+document
+  .querySelector("#dest_details_form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+
+// TODO - We need to retrieve the values of what the user inputs in all the elements and then store them in variables
+
+// TODO - Create a form reset for a new input
+
+// TODO - Create a destination card using the form element values
+
+// TODO -

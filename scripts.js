ad50ff8a1b194b1a5eef543137386e571700aa81
ngonpaul <pmgonzalez05@outlook.com>
2022-07-18 02:18:33 -0700

    quick change in js file

--- scripts.js
+++ scripts.js
@@ -1,31 +1,33 @@
 // TODO - We need to listen to what is being submitted in the form
 document
-  .querySelector("#dest_details_form")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
+  .querySelector("#dest_detail_form")
+  .addEventListener("submit", formSubmissionData);
 
-    // TODO - We need to retrieve the values of what the user inputs in all the elements and then store them in variables
-    var destName = event.target.elements["dest_name"].value;
-    var destLocation = event.target.elements["location"].value;
-    var destPhoto = event.target.elements["photo"].value;
-    var destDescription = event.target.elements["description"].value;
+function formSubmissionData(event) {
+  event.preventDefault();
 
-    // TODO - Create a form reset for a new input
-    resetFormValues(event.target);
+  // TODO - We need to retrieve the values of what the user inputs in all the elements and then store them in variables
+  var destName = event.target.elements["dest_name"].value;
+  var destLocation = event.target.elements["location"].value;
+  var destPhoto = event.target.elements["photo"].value;
+  var destDescription = event.target.elements["description"].value;
 
-    // TODO - Create a destination card using the form element values
-    var destCard = createDestinationCard(
-      destName,
-      destLocation,
-      destPhoto,
-      destDescription
-    );
+  // TODO - Create a form reset for a new input
+  resetFormValues(event.target);
 
-    var destContainer = document.querySelector("#dest_container");
+  // TODO - Create a destination card using the form element values
+  var destCard = createDestinationCard(
+    destName,
+    destLocation,
+    destPhoto,
+    destDescription
+  );
 
-    // TODO - Append my destCard in the dest_container div
-    document.querySelector("dest_container").appendChild(destCard);
-  });
+  var destContainer = document.querySelector("#dest_container");
+
+  // TODO - Append my destCard in the dest_container div
+  document.querySelector("#dest_container").appendChild(destCard);
+}
 
 // TODO - Reset all values in the forms
 
